3c0e6149d4491b7f55ceb2557d6581c714fdf225
Gustavo Jordin Martinez Orosco <gustavojordin.gjmo@gmail.com>
2024-09-15 00:25:47 -0600

    avances en js

--- script.js
+++ script.js
@@ -5,3 +5,24 @@ function agregarValor(valor) {
     operacionActual += valor;
     pantalla.value = operacionActual;
 }
+
+const botones = document.querySelectorAll('button');
+botones.forEach(boton => {
+    boton.addEventListener('click', function() {
+        const valor = this.textContent;
+
+        if (valor === 'c') {
+            operacionActual = '';
+            pantalla.value = '';
+        } else if (valor === '=') {
+            try {
+                pantalla.value = eval(operacionActual);
+                operacionActual = '';
+            } catch (e) {
+                pantalla.value = 'error';
+            }
+        } else {
+            agregarValor(valor);
+        }
+    });
+});
